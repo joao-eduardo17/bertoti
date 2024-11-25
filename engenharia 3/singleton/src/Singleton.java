52840c79450f89fe929b18f44b2f403475dabb84
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2024-11-25 08:59:41 -0300

    fix

---
+++ engenharia 3/singleton/src/Singleton.java
@@ -0,0 +1,16 @@
+public class Singleton {
+    private static Singleton instanciaUnica;
+
+    private Singleton() {}
+
+    public static Singleton getInstanciaUnica() {
+        if (instanciaUnica == null) {
+            instanciaUnica = new Singleton();
+        }
+        return instanciaUnica;
+    }
+
+    public void mensagem() {
+        System.out.println("Instância única criada!");
+    }
+}
